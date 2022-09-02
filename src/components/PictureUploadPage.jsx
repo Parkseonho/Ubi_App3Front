647ce780062031976935a064616c31b05f0e6b92
Parkseonho <tjsgh2103@gmail.com>
2022-09-02 13:42:41 +0900

    PictureUpload -2

--- src/components/PictureUploadPage.jsx
+++ src/components/PictureUploadPage.jsx
@@ -1,31 +1,38 @@
 import React from "react";
+import "../index.css"
 
 const PictureUploadPage = () => {
   return (
     <>
-    <div
+      <div
         style={{
           backgroundColor: "#CCF2F4",
           height: 60
 
         }} >
-       
+        <strong> 사진 업로드 </strong>
       </div>
-      <div className="PageHeight">
+      {/* 내용 */}
+      <div className="PageHeight py-auto">
+        {/* 사진/영상 업로드 부분 */}
         <div class="form-control w-full max-w-xs">
           <label class="label">
             <strong class="label-text">사진/영상</strong>
           </label>
           <input type="file" className="form-control" ></input>
         </div>
+        {/* 내용 업로드 부분 */}
         <div class="form-control w-full max-w-xs">
           <label class="label">
             <strong class="label-text">내용</strong>
           </label>
           <textarea type="text" placeholder="내용" class="input input-bordered w-full " />
         </div>
-        <button class="btn mr-2">취소</button>
-        <button class="btn">확인</button>
+        {/* 확인/취소 버튼 */}
+        <div>
+          <button class="btn mr-2">취소</button>
+          <button class="btn">확인</button>
+        </div>
       </div>
 
     </>
