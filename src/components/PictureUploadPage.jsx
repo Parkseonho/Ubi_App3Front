5f814fb546620abeaccd6f99958bbfc508e6f9e0
Parkseonho <tjsgh2103@gmail.com>
2022-09-05 13:41:10 +0900

    파일 업로드 부분

--- src/components/PictureUploadPage.jsx
+++ src/components/PictureUploadPage.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css"
+import axios from "axios";
 
-const PictureUploadPage = () => {
+const PictureUploadPage = ({ post, setPost, nextId}) => {
+  const [content, setContent] = useState("");
   return (
     <>
       <div
@@ -12,27 +14,60 @@ const PictureUploadPage = () => {
         }} >
         <strong> 사진 업로드 </strong>
       </div>
+
       {/* 내용 */}
       <div className="PageHeight py-auto">
-        {/* 사진/영상 업로드 부분 */}
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <strong class="label-text">사진/영상</strong>
-          </label>
-          <input type="file" className="form-control" ></input>
-        </div>
-        {/* 내용 업로드 부분 */}
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <strong class="label-text">내용</strong>
-          </label>
-          <textarea type="text" placeholder="내용" class="input input-bordered w-full " />
-        </div>
-        {/* 확인/취소 버튼 */}
-        <div>
-          <button class="btn mr-2">취소</button>
-          <button class="btn">확인</button>
-        </div>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const data = await axios({
+              url: "http://localhost:8083/multi-file",
+              method: "POST",
+              data: { content },
+            });
+            setPost(data.content);
+            nextId.current++;
+            setContent("");
+          }}          
+        >
+          {/* 사진/영상 업로드 부분 */}
+          {/*
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <strong class="label-text">사진/영상</strong>
+            </label>
+            <input type="file"
+              className="form-control"
+              value={files}
+              onChange={(e) => {
+                setFiles(e.target.value);
+              }}
+            ></input>
+          </div>
+          */
+          }
+          {/* 내용 업로드 부분 */}
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <strong class="label-text">내용</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="내용"
+              class="input input-bordered w-full "
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            >
+            </input>
+          </div>
+          {/* 확인/취소 버튼 */}
+          <div>
+            <button class="btn mr-2">취소</button>
+            <button type="submit" class="btn">확인</button>
+          </div>
+        </form>
       </div>
 
     </>
