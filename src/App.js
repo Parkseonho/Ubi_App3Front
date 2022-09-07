3ed6f7554335f8576272fecc2659b5ee3cc44089
Parkseonho <tjsgh2103@gmail.com>
2022-09-07 10:34:06 +0900

    todoList관련 내용 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 
-/*
 import React, { useEffect, useRef, useState } from "react";
 import "./index.css";
-import HomePage from "./components/HomePage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 import axios from "axios";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    API 호출 코드
+    /**API 호출 코드*/
     const getData = async () => {
       const todos = await axios({
         url: "http://localhost:8083/todos",
         method: "GET",
       });
-      console.log("todos", todos);
       setTodos(todos.data);
     };
 
@@ -26,15 +23,11 @@ function App() {
   const nextId = useRef(4);
 
   return (
-    <div className="max-w-4xl mx-auto">
-      <Header/>
-      <div className="max-w-4xl mx-auto my-4">
-      <HomePage/>
-      </div>
-      <Footer/>
+    <div className="max-w-4xl mx-auto mt-4">
+      <TodoInput todos={todos} setTodos={setTodos} nextId={nextId} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
 
 export default App;
-*/
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import ReactDOM from "react-dom/client";
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Home from "./page/Home";
+import App from "./App";
 import TestPage from "./page/TestPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <TestPage />
+     <App />
   </React.StrictMode>
 );
